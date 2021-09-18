a18a0fa4a4425db4d189ef2ef0dcce486923bca3
Prince Kumar Kanoujiya <iprincekumark@gmail.com>
2021-09-18 05:33:42 +0530

    initial commit

---
+++ Subtract-the-Product-and-Sum-of-Digits-of-an-Integer.cpp
@@ -0,0 +1,46 @@
+Subtract the Product and Sum of Digits of an Integer
+
+/*
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+*/
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+ 
+
+Constraints:
+
+1 <= n <= 10^5
+
+#Solution :-
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int dig, sod=0, pod=1;
+        if(n<1 || n>(100000))
+            return 0;
+        else {
+            while(n>0) {
+                dig = n%10;
+                sod = sod + dig;
+                pod = pod * dig;
+                n = n/10;
+            }
+            return pod-sod;
+        }
+    }
+};
